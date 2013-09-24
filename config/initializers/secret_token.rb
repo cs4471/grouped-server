c429a615a8b478209b1d8c86b7a6f49f0d1d4602
Sebastian Thimmig <thimmig.2@osu.edu>
2013-09-24 14:51:06 +0000

    Barebones Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupedServer::Application.config.secret_token = 'c17df1466eb62373e316e795eb2c4be8f051c72dd04b9a46aa9b6d5b278b988341de0ca6ccdea6354e9c4fa30b480566efbf7eb8f7b11f958adc66add52fde11'
